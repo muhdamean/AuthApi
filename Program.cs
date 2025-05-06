645fde3597496cdeec7ca14bc35f8a95fd7c4224
Muhammad Aminu Aliyu <Muhdaliyu1@gmail.com>
2025-05-06 22:52:07 +0100

    Add project files

---
+++ Program.cs
@@ -0,0 +1,133 @@
+using AuthApi.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using Serilog.Events;
+using System.Text;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .WriteTo.Console()
+    .CreateBootstrapLogger(); // Early logger for startup issues
+
+try
+{
+    Log.Information("Starting web host...");
+    var builder = WebApplication.CreateBuilder(args);
+    const string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+    Log.Logger = new LoggerConfiguration()
+   .ReadFrom.Configuration(builder.Configuration)
+   .Enrich.FromLogContext()
+   .CreateLogger();
+    // Replace default logging with Serilog
+    builder.Host.UseSerilog();
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy(name: MyAllowSpecificOrigins,
+            builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+    });
+    // Add services to the container.
+    builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
+    builder.Services.AddSingleton<TokenService>();
+    builder.Services.AddControllers();
+    builder.Services.AddOptions();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+       .AddJwtBearer(options =>
+       {
+           options.TokenValidationParameters = new TokenValidationParameters
+           {
+               ValidateIssuer = true,
+               ValidateAudience = true,
+               ValidateIssuerSigningKey = true,
+               ValidateLifetime = true,
+               ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+               ValidAudience = builder.Configuration["JwtSettings:Audience"],
+               IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"] ?? string.Empty)),
+           };
+       });
+
+
+    builder.Services.AddSwaggerGen(options =>
+    {
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "Auth Demo API",
+            Version = "v1",
+            Description = "Demo Authorization API",
+        });
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please enter a valid token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+        });
+        // Grouping API endpoints by tags
+        //options.DocInclusionPredicate((docName, apiDesc) =>
+        //{
+        //    var controllerActionDescriptor = apiDesc.ActionDescriptor;
+        //    var routeGroup = controllerActionDescriptor.RouteValues["controller"];
+        //    return true; // Include all routes by default, or customize this logic as needed
+        //});
+    });
+
+
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    //if (app.Environment.IsDevelopment())
+    //{
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    //}
+
+    app.UseHttpsRedirection();
+    // Logs info about HTTP requests automatically
+    app.UseSerilogRequestLogging();
+    app.UseCors(MyAllowSpecificOrigins);
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application failed to start");
+}
+finally
+{
+    //Console.ReadKey();
+    Log.CloseAndFlush();
+}
